c3d110e972e34188feba3222e7e52102f71cf5c2
x1mgathier <x1mgathier@C115462.eur.corp.vattenfall.com>
2015-04-17 14:38:16 +0200

    DEMO2

---
+++ com/vattenfall/git/Sample.java
@@ -0,0 +1,11 @@
+package com.vattenfall.git;
+
+public class Sample {
+    public Sample() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        Sample sample = new Sample();
+    }
+}
